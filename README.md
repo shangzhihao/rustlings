88b14dd0f6670c88b56de5c634d97cc7b0956e2f
shangzhihao <zhihao.shang@gmail.com>
2026-05-04 11:34:50 +0200

    3 chapters done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rustlings Workspace
+
+This repository is a local Rustlings practice workspace.
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,7 +11,9 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
-
+fn calculate_price_of_apples(n: u32) -> u32 {
+    if n <= 40 { n * 2 } else { n }
+}
 fn main() {
     // You can optionally experiment here.
 }
